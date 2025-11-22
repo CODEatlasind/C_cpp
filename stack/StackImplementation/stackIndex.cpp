04d20e82e5a474b52fa85931be5dba7e02196161
ShashankShekhar <shashankshekharsn042@gmail.com>
2025-11-23 03:36:57 +0530

    Added implementation of stack using linkedlist

--- stack/StackImplementation/stackIndex.cpp
+++ stack/StackImplementation/stackIndex.cpp
@@ -1,27 +1,29 @@
 #include <bits/stdc++.h>
 #include "stackUsingArray.cpp"
+#include "stackUsingLinkedList.cpp"
 using namespace std;
 
 int main()
 {
-    stackUsingArray stack(5);
+    // Stack using array
+    // stackUsingArray stack(5);
+    // stack using linked list
+    stackUsingLinkedList stack;
 
     // stack.pop();
     cout << stack.isEmpty() << endl;
     stack.push(1);
-    stack.push(1);
-    stack.push(1);
-    stack.push(1);
+    stack.push(4);
+    stack.push(2);
+    stack.push(5);
     // stack.push(1);
     cout << stack.top() << endl;
     stack.push(3);
     // stack.push(3);
-    cout << stack.size() << endl;
+    cout << stack.getSize() << endl;
     stack.pop();
     stack.pop();
-    stack.pop();
-    stack.pop();
-    stack.pop();
+
     cout << stack.isEmpty() << endl;
     // stack.pop();
 }
