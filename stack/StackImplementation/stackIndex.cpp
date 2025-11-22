cbbd7059312dd9c97a99419c74439210fb28685a
Shashank Shekhar <77682631+CODEatlasind@users.noreply.github.com>
2025-11-23 03:53:36 +0530

    Revert "Stack implementation using linked list (#1)"
    This reverts commit 703031b529f5a82966f4e38df58f8aab51d27718.

--- stack/StackImplementation/stackIndex.cpp
+++ stack/StackImplementation/stackIndex.cpp
@@ -1,29 +1,27 @@
 #include <bits/stdc++.h>
 #include "stackUsingArray.cpp"
-#include "stackUsingLinkedList.cpp"
 using namespace std;
 
 int main()
 {
-    // Stack using array
-    // stackUsingArray stack(5);
-    // stack using linked list
-    stackUsingLinkedList stack;
+    stackUsingArray stack(5);
 
     // stack.pop();
     cout << stack.isEmpty() << endl;
     stack.push(1);
-    stack.push(4);
-    stack.push(2);
-    stack.push(5);
+    stack.push(1);
+    stack.push(1);
+    stack.push(1);
     // stack.push(1);
     cout << stack.top() << endl;
     stack.push(3);
     // stack.push(3);
-    cout << stack.getSize() << endl;
+    cout << stack.size() << endl;
     stack.pop();
     stack.pop();
-
+    stack.pop();
+    stack.pop();
+    stack.pop();
     cout << stack.isEmpty() << endl;
     // stack.pop();
 }
